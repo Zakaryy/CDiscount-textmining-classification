2ec9cb1fa2bdc9ffdc87314ac151437bce3bad4b
Zakary <30469684+Zakaryy@users.noreply.github.com>
2018-06-23 19:27:18 +0200

    add dimensions file
    Co-Authored-By: dylan94290 <dylan94290@users.noreply.github.com>

---
+++ dimensions.py
@@ -0,0 +1,10 @@
+dimension = {
+        'dim_heat' : ['c', 'celsius', 'f', 'fahrenheit', 'fahrenheits'],
+        'dim_height' : ['pouce', 'pouces', 'inch', 'km', 'm', 'cm' ,'metre', 'decimetre', 'centimetre', 'm', 'dm', 'cm', 'mm', 'ft', 'kilometre', 'mi',  'mille', 'foot', 'feetkm','metres', 'decimetres', 'centimetres', 'feetkms', 'kilometres', 'milles'],
+        'dim_weight' : ['t', 'kg', 'hg', 'g', 'dg', 'cg', 'mg', 'tonne', 'kilogramme', 'hectogramme', 'gramme', 'decigramme', 'centigramme', 'milligramme', 'calorie', 'kilocalorie','tonnes', 'kilogrammes', 'hectogrammes', 'grammes', 'decigrammes', 'centigrammes', 'milligrammes','calories', 'kilocalories'],
+        'dim_volume' : ['l', 'dl', 'cl', 'ml', 'litre', 'decilitre', 'centilitre', 'millilitre', 'litres', 'decilitres', 'centilitres', 'millilitres'],
+        'dim_memory' : ['ko', 'mo', 'go', 'to', 'octet', 'kilobyte', 'megaoctet', 'gigaoctet', 'teraoctet',  'octets', 'megaoctets', 'gigaoctets', 'teraoctets','kilobytes'],
+        'dim_frequency' : ['hz', 'khz', 'mhz', 'ghz', 'hertz', 'kilohertz', 'megahertz', 'gigahertz', 'hertzs', 'kilohertzs', 'megahertzs', 'gigahertzs'],
+        'dim_energy' : ['kj', 'al', 'kcal', 'wh', 'kwh', 'joule', 'kilojoule','watt', 'watts', 'kilowatt', 'kilowatts', 'joules', 'kilojoules'],
+        "dim_time"  :  ['millisecondes', 'milliseconde', 'secondes','seconde', 'minute', 'minutes', 'heures','heure', 'min', 'sec', 'ms']
+        }
